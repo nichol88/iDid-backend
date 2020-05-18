5d3321aa959e6988018b98e929d52d4a7fb7102c
Brian Nicholls <nichol88@gmail.com>
2020-05-17 22:52:14 -0400

    add feature: add counters

--- app/controllers/api/v1/counters_controller.rb
+++ app/controllers/api/v1/counters_controller.rb
@@ -1,8 +1,26 @@
 class API::V1::CountersController < ApplicationController
 
   def index
-    # byebug
-    counters = Counter.all
-    render json: counters
+    render json: Counter.all
   end
+
+  def create
+    counter = Counter.create(name: params[:counterFormData][:name], kind: 'default')
+
+    counter[:kind] = 'weighted' if params[:counterFormData][:weighted]
+    counter[:kind] = 'timed' if params[:counterFormData][:timed]
+
+    if counter.save
+      render json: Counter.all
+    else
+      render json: {server_message: "Could not create counter! #{counter.errors.full_messages.to_sentence}"}
+    end
+  end
+
+  private
+
+  def counter_params
+    params.require(:counterFormData).permit(:name, :weighted, :timed, :kind)
+  end
+
 end
