6b502b216f21bb243b1426c8c9a0874c1b9581c2
Brian Nicholls <nichol88@gmail.com>
2020-05-19 15:26:20 -0400

    bug fix, CORS issue: add origin for Netlify hosting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://silly-almeida-f17772.netlify.app'
 
     resource '*',
       headers: :any,
