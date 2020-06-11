3d3c61436f37cafe25b63550136e05a295664261
Brian Nicholls <nichol88@gmail.com>
2020-06-10 22:51:04 -0400

    add scope methods and PG queries for leaderboards

--- app/models/action.rb
+++ app/models/action.rb
@@ -3,4 +3,13 @@ class Action < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :routine, optional: true
 
+  scope :today, -> {Action.where('created_at >= ?', 24.hours.ago)}
+  scope :week, -> {Action.where('created_at >= ?', 1.week.ago)}
+  scope :month, -> {Action.where('created_at >= ?', 1.month.ago)}
+
+  scope :recent, -> {Action.order('created_at DESC limit 10')}
+
+  
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,24 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :fname, presence: true
   validates :lname, presence: true
+
+  # returns each counter's rep leader, all-time
+  # returns each counter's rep leader, all-time
+  def self.top_all_time
+    records = ActiveRecord::Base.connection.execute('SELECT SUM(reps), counter_id, user_id
+      FROM actions
+      INNER JOIN counters ON counters.id = counter_id
+      INNER JOIN users ON users.id = user_id
+      GROUP BY user_id, counter_id;')
+    records.to_a.map { |record|
+      record[:allTimeLeader] = User.find_by(id: record['user_id']).fname
+      record[:counter] = Counter.find_by(id: record['counter_id']).name
+      record
+    }
+    # byebug
+    # puts 'beep boop'
+
+  end
+
+
 end
