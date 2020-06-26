1af182fd2964eae000327162c2814f8de24d4455
nichol88 <nichol88@gmail.com>
2020-06-26 16:00:03 -0400

    refactor action.total_reps into scope method

--- app/models/action.rb
+++ app/models/action.rb
@@ -7,6 +7,9 @@ class Action < ApplicationRecord
   scope :week, -> {Action.where('created_at >= ?', 1.week.ago)}
   scope :month, -> {Action.where('created_at >= ?', 1.month.ago)}
 
+
+  # /models/action.rb
+  scope :total_reps, -> {sum(:reps)}
   scope :recent, -> {Action.order('created_at DESC limit 10')}
 
   def self.since(datetime)
@@ -17,8 +20,8 @@ class Action < ApplicationRecord
     Action.where('user_id = ? AND created_at >= ? ', self.user.id, datetime)
   end
 
-  def self.total_reps(actions)
-    actions.sum(:reps)
-  end
+  # def self.total_reps(actions)
+  #   actions.sum(:reps)
+  # end
 
 end
