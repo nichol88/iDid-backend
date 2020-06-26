cc7adde6d899707ccd02a38d613d88ac56ff4e97
nichol88 <nichol88@gmail.com>
2020-06-26 16:04:06 -0400

    add leader method to Counter

--- app/models/counter.rb
+++ app/models/counter.rb
@@ -1,29 +1,33 @@
 class Counter < ApplicationRecord
+  include ActiveModel::Serialization
+
   has_many :actions
 
   validates :name, presence: true, uniqueness: true
   validates_inclusion_of :kind, in: ['default', 'weighted', 'timed']
 
-  # return User and total reps of leader
-  def leader(datetime)
-    # get actions for this counter
-    actions = self.actions.where('created_at >= ? ', datetime)
+  # /models/counter.rb
 
+  # returns hash with leader (user) and total reps
+  def leader(datetime = DateTime.parse('1/1/2020'))
     # get unique users having actions for this counter
-    unique_users = actions.pluck(:user).uniq
+    unique_users = self.actions.since(datetime).pluck(:user_id).uniq
 
-    # get sum of reps for each user within action set
-    # result will be an array of hashes, e.g. [{user_id, reps}...]
-    result = []
-    unique_users.each do |user|
-      reps = actions.where('user_id = ?', user.id).sum(:reps)
-      result.push[{user_id: user.id, reps: reps}]
+    # get total_reps in this time frame for each unique user
+    # result will be an array of hashes, e.g. [{user, reps}...]
+    result = unique_users.map do |user_id|
+      user = User.find(user_id)
+      reps = user.actions.since(datetime).total_reps
+      {user: {id: user.id, name: user.name}, reps: reps}
     end
 
     # return the hash with most reps
-    result.max_by { |obj| obj.reps }
+    result = result.max_by { |user_hash_map| user_hash_map[:reps] }
 
-    # returns {user_id, reps}
+    # to properly serialize the data, we need to handle for nil
+    result ? result : {error: "No data for selected time range"}
+
+    # returns e.g. {:user=>{:id=>1, :name=>"Scooby Doo"}, :reps=>30}
   end
 
 
