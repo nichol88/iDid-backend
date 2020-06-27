4163d152520e6150d5380268d99246030a115115
nichol88 <nichol88@gmail.com>
2020-06-27 01:23:36 -0400

    remove unnecessary/old code

--- app/controllers/api/v1/actions_controller.rb
+++ app/controllers/api/v1/actions_controller.rb
@@ -1,7 +1,4 @@
 class API::V1::ActionsController < ApplicationController
-
-  # before_action :redirect_if_not_logged_in
-
   def index
     if params[:id]
       actions = Action.where(user_id: params[:id])
@@ -9,7 +6,6 @@ class API::V1::ActionsController < ApplicationController
     else
       render json: {server_message: 'Could not get actions for user!'}
     end
-
   end
 
   def create
@@ -17,14 +13,12 @@ class API::V1::ActionsController < ApplicationController
     counter = Counter.find_by(id: params[:counter_id])
     if counter
         action.counter_id = counter.id
-        action.name = counter.name
     end
     if action.save
       render json: action
     else
       render json: {server_message: 'Unable to create Action!'}
     end
-
   end
 
   def leaders
@@ -34,7 +28,7 @@ class API::V1::ActionsController < ApplicationController
   private
 
   def action_params
-    params.permit(:reps, :user_id, :name, :counter_id, :weight, :weight_unit, :time_unit)
+    params.permit(:reps, :user_id, :counter_id, :weight)
   end
 
 end
