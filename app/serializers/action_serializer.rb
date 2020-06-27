a2f96e6730f2b606ed17ce9b4b4c988a5bff81ae
nichol88 <nichol88@gmail.com>
2020-06-27 01:25:25 -0400

    remove old code from serializer

--- app/serializers/action_serializer.rb
+++ app/serializers/action_serializer.rb
@@ -8,14 +8,16 @@ class ActionSerializer < ActiveModel::Serializer
 
   def counter
     {name: object.counter.name,
-      kind: object.counter.kind}
+      kind: object.counter.kind,
+      unit: object.counter.measurement_unit
+    }
   end
 
   attribute :weight, if: :is_weighted?
-  attribute :weight_unit, if: :is_weighted?
+  # attribute :weight_unit, if: :is_weighted?
 
   # attribute :time_duration, if: :is_timed?
-  attribute :time_unit, if: :is_timed?
+  # attribute :time_unit, if: :is_timed?
 
   def is_timed?
     object.counter.kind == 'timed' ? true : false
