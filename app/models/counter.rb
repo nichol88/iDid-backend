55e53884796b5b267e5e188855e7561271594f3c
Brian (Home) <nichol88@gmail.com>
2020-07-21 19:47:32 -0400

    fix syntax on result.push

--- app/models/counter.rb
+++ app/models/counter.rb
@@ -10,18 +10,18 @@ class Counter < ApplicationRecord
     actions = self.actions.where('created_at >= ? ', datetime)
 
     # get unique users having actions for this counter
-    unique_users = actions.pluck(:user).uniq
+    unique_users = actions.pluck(:user_id).uniq
 
     # get sum of reps for each user within action set
     # result will be an array of hashes, e.g. [{user_id, reps}...]
     result = []
-    unique_users.each do |user|
-      reps = actions.where('user_id = ?', user.id).sum(:reps)
-      result.push[{user_id: user.id, reps: reps}]
+    unique_users.each do |id|
+      reps = actions.where('user_id = ?', id).sum(:reps)
+      result.push({user_id: id, reps: reps})
     end
 
     # return the hash with most reps
-    result.max_by { |obj| obj.reps }
+    result.max_by { |obj| obj[:reps] }
 
     # returns {user_id, reps}
   end
