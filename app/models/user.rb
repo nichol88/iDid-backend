502a303916641f07c24f45b80fc95cc850115d52
Brian (Home) <nichol88@gmail.com>
2020-07-22 21:59:42 -0400

    remove old code

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,25 +8,10 @@ class User < ApplicationRecord
   validates :fname, presence: true
   validates :lname, presence: true
 
-  # returns each counter's rep leader, all-time
-  # def self.top_all_time
-  #   records = ActiveRecord::Base.connection.execute('SELECT SUM(reps), counter_id, user_id
-  #     FROM actions
-  #     INNER JOIN counters ON counters.id = counter_id
-  #     INNER JOIN users ON users.id = user_id
-  #     GROUP BY user_id, counter_id;')
-  #
-  #   records.to_a.map { |record|
-  #     record[:allTimeLeader] = User.find_by(id: record['user_id']).fname
-  #     record[:counter] = Counter.find_by(id: record['counter_id']).name
-  #     record
-  #   }
-  # end
-
   def self.leaders_since(datetime)
     # get unique counters for this time frame
     actions = Action.since(datetime)
-    unique_counters = actions.pluck(:counter)
+    unique_counters = actions.pluck(:counter).uniq
 
     # map unique counters to leader hash
     unique_counters.map do |counter|
@@ -34,10 +19,7 @@ class User < ApplicationRecord
     end
 
     # returns array of hashes
-
   end
-
-  # /models/user.rb
 
   def counter_actions(counter_id)
     self.actions.where("counter_id = ?", counter_id)
@@ -46,8 +28,5 @@ class User < ApplicationRecord
   def name
     "#{fname} #{lname}"
   end
-
-
-
 
 end
