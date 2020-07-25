930c31e0a38531e7c7a58f2a02303e5fab37fffb
Brian (Home) <nichol88@gmail.com>
2020-07-25 16:14:44 -0400

    add tests using MiniTest

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  # setup do
+  #   @user = users(:joseph)
+  # end
+
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # 
+  # self.use_instantiated_fixtures = true
+  #
+  # setup do
+  #   # give jonathan a password
+  #   @jonathan.password_digest =  BCrypt::Password.create('testpassword', cost: 5)
+  # end
+  #
+  # test "user is created with all attributes" do
+  #   assert @jonathan.fname
+  #   assert @jonathan.lname
+  #   assert @jonathan.email
+  #   assert @jonathan.password_digest
+  # end
+  #
+  # test "user can be saved to database" do
+  #   assert @jonathan.save
+  # end
+
+end
