c2e43a63ed5d3e49c60f8d0caf2b22983c1e75c9
Brian (Home) <nichol88@gmail.com>
2020-07-27 11:24:54 -0400

    fix attribute names in seeds

--- db/migrate/20200514041823_create_counters.rb
+++ db/migrate/20200514041823_create_counters.rb
@@ -2,7 +2,7 @@ class CreateCounters < ActiveRecord::Migration[6.0]
   def change
     create_table :counters do |t|
       t.string :name
-      t.string :type
+      t.string :kind
 
       t.timestamps
     end
--- db/migrate/20200514043431_create_routines.rb
+++ db/migrate/20200514043431_create_routines.rb
@@ -2,7 +2,7 @@ class CreateRoutines < ActiveRecord::Migration[6.0]
   def change
     create_table :routines do |t|
       t.string :name
-      t.string :type
+      t.string :kind
       t.integer :user_id
 
       t.timestamps
